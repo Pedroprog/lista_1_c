a8addbd01616426889c67b061792b5b42f8f88f8
Pedroprog <38636285+Pedroprog@users.noreply.github.com>
2018-04-22 20:59:43 -0300

    Add files via upload

---
+++ Q_01.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* 1. Faca um programa que leia um numero inteiro e o imprima */
+
+int main() {
+	int a;
+	printf("digite um valor: ");
+	scanf("%d",&a);
+	printf("o valor digitado foi:%d",a);
+
+    return 0;
+}
