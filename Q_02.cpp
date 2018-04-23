9d4eea6f7501e9c156b748347715e704fc0520fc
Pedroprog <38636285+Pedroprog@users.noreply.github.com>
2018-04-22 21:21:48 -0300

    Add files via upload

---
+++ Q_02.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*2. Faça um programa que leia um numero real e o imprima*/
+
+int main(){
+	float a;
+	printf("Digite um numero: ");
+	scanf("%f",&a);
+	printf("O valor digitado foi:%f",a);
+	
+	return (0);
+	
+	
+}
