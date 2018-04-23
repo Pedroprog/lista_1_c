bcb5ed1cd4337e398a4ec856d9062609f9fb7001
Pedroprog <38636285+Pedroprog@users.noreply.github.com>
2018-04-22 22:44:13 -0300

    Add files via upload

---
+++ Q_04.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* 4. Leia um numero real e imprima o resultado do quadrado desse numero. */
+
+int main() {
+	float a;
+	float x;
+	printf("Digite o valor de a:  ");
+	scanf("%f",&a);
+	x=a*a;
+	printf("o valor de a vale:%f",x);
+	return 0;
+}
